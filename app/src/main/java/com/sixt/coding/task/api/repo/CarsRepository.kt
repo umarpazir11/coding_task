b8c4840874716a296a3adaa01110c055d1b7345d
Valtrans <valtransvaletparking@gmail.com>
2019-08-04 15:02:35 +0400

    Project Updated with 18 percent test coverage

--- app/src/main/java/com/sixt/coding/task/api/repo/CarsRepository.kt
+++ app/src/main/java/com/sixt/coding/task/api/repo/CarsRepository.kt
@@ -1,6 +1,7 @@
 package com.sixt.coding.task.api.repo
 
 import com.sixt.coding.task.api.NetworkServices
+
 class CarsRepository(private val sixtApiService: NetworkServices) {
 
     fun getCars() = this.sixtApiService.cars!!
